2132590043f6c79f99f35733dc9926ba032fca4f
David-B-A <dabelloa@unal.edu.co>
2022-09-09 16:27:14 -0500

    Fix/Fixing bug that takes real curve plain to the end

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -1,4 +1,4 @@
 import powerbiVisualsApi from "powerbi-visuals-api";
 import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var sCurve68E291660A80407EA125459727A40E5F_DEBUG: IVisualPlugin;
-export default sCurve68E291660A80407EA125459727A40E5F_DEBUG;
+declare var sCurve68E291660A80407EA125459727A40E5F: IVisualPlugin;
+export default sCurve68E291660A80407EA125459727A40E5F;
